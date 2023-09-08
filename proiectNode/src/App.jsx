681a886f40b04f045da86ed755c5a20fea285a2d
RazvanMocanu1 <razvanmocanu2009@gmail.com>
2023-09-08 17:21:59 +0300

    aluat

--- proiectNode/src/App.jsx
+++ proiectNode/src/App.jsx
@@ -10,12 +10,17 @@ const MesajSecundar = () => {
   return <h1>𝔄𝔠𝔢𝔞𝔰𝔱𝔞 𝔢𝔰𝔱𝔢 𝔬 𝔦𝔪𝔞𝔤𝔦𝔫𝔢 𝔠𝔞𝔯𝔢 𝔱𝔯𝔢𝔟𝔢 𝔰𝔞 𝔦𝔱𝔦 𝔡𝔞𝔦 𝔰𝔢𝔞𝔪𝔞 𝔠𝔢 𝔞𝔫𝔲𝔪𝔢 𝔰𝔢 𝔞𝔣𝔩𝔞 𝔦𝔫 𝔢𝔞</h1>;
 };
 
+const Hint = () => {
+  return <h6>Hint : este un Material din care se fac gogosile</h6>
+}
+
 const App = () => {
   return (
     <div>
       <NumeSite/>
       <DoughIcon/>
       <MesajSecundar/>
+      <Hint/>
     </div>
   );
 };
