b43831fd2828c4d0ee81b1e465714a3ce81507b2
RazvanMocanu1 <razvanmocanu2009@gmail.com>
2023-09-08 16:53:57 +0300

    mesaj secundar

--- proiectNode/src/App.jsx
+++ proiectNode/src/App.jsx
@@ -2,10 +2,20 @@ const NumeSite = () => {
   return <h1>Bun venit pe site-ul meu</h1>;
 };
 
+const DoughIcon = () => {
+  return <img src="DoughIcon.png" width={250} height={250}></img>
+}
+
+const MesajSecundar = () => {
+  return <h1>𝔄𝔠𝔢𝔞𝔰𝔱𝔞 𝔢𝔰𝔱𝔢 𝔬 𝔦𝔪𝔞𝔤𝔦𝔫𝔢 𝔠𝔞𝔯𝔢 𝔱𝔯𝔢𝔟𝔢 𝔰𝔞 𝔦𝔱𝔦 𝔡𝔞𝔦 𝔰𝔢𝔞𝔪𝔞 𝔠𝔢 𝔞𝔫𝔲𝔪𝔢 𝔰𝔢 𝔞𝔣𝔩𝔞 𝔦𝔫 𝔢𝔞</h1>;
+};
+
 const App = () => {
   return (
     <div>
       <NumeSite/>
+      <DoughIcon/>
+      <MesajSecundar/>
     </div>
   );
 };
