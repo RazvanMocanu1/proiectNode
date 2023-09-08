ccbe3ed6c1c8641bcc053a0affdd5a0c987f809b
RazvanMocanu1 <razvanmocanu2009@gmail.com>
2023-09-08 15:56:49 +0300

    messaj

--- proiectNode/src/App.jsx
+++ proiectNode/src/App.jsx
@@ -1,8 +1,13 @@
+const numeSite = () => {
+  return <h1>Bun venit pe site-ul meu</h1>;
+};
 
-
-function App() {
-
-  return 
-}
+const App = () => {
+  return (
+    <div>
+      <numeSite></numeSite>
+    </div>
+  );
+};
 
 export default App
