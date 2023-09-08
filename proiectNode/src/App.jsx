f4a60de7fdff573972e1eb582d802ef32a14387f
RazvanMocanu1 <razvanmocanu2009@gmail.com>
2023-09-08 19:10:26 +0300

    final

--- proiectNode/src/App.jsx
+++ proiectNode/src/App.jsx
@@ -42,6 +42,51 @@ const Hint3 = () => {
   return <h6>Hint : are numai 5 litere</h6>
 };
 
+const RubikCube = () => {
+  return <img src="cubRubik.png" width={250} height={250}></img>
+};
+
+const MesajCubRubik = () => {
+  return <h3>Acesta este un cub rubik , oare cate patrate se afla pe o singura fata a acestuia</h3>
+}
+
+const Hint4 = () => {
+  return <h6>Hint : gandeste inafara cutiei</h6>
+}
+
+const Spaghetti = () => {
+  return <img src="spaghetti.jpg" width={250} height={250}></img>
+};
+
+const MesajSpaghetti = () => {
+  return <h3>Aceasta este o reteta anume de paste si trebe sa iti dai seama care reteta este mai exact</h3>
+}
+
+const Hint5 = () => {
+  return <h6>Hint : acesta reteta contine multa carne</h6>
+}
+
+const RaspunsuriCorecte = () => {
+  return <h2>Raspunsurile corecte erau : </h2>
+}
+
+const Raspunsul1 = () => {
+  return <h2> - Dough / Aluat </h2>
+}
+
+const Raspunsul2 = () => {
+  return <h2> - Aroma de capsune </h2>
+}
+const Raspunsul3 = () => {
+  return <h2> - mesaj </h2>
+}
+const Raspunsul4 = () => {
+  return <h2> - 14 patrate per culoare </h2>
+}
+const Raspunsul5 = () => {
+  return <h2> - Paste Bologneze "p.s : am cautat pe net cum se scrie corect" </h2>
+}
+
 const App = () => {
   return (
     <div>
@@ -57,6 +102,21 @@ const App = () => {
       <Mesaj/>
       <Mesaj2/>
       <Hint3/>
+      <Spatiu/>
+      <RubikCube/>
+      <MesajCubRubik/>
+      <Hint4/>
+      <Spatiu/>
+      <Spaghetti/>
+      <MesajSpaghetti/>
+      <Hint5/>
+      <Spatiu/>
+      <RaspunsuriCorecte/>
+      <Raspunsul1/>
+      <Raspunsul2/>
+      <Raspunsul3/>
+      <Raspunsul4/>
+      <Raspunsul5/>
     </div>
   );
 };
